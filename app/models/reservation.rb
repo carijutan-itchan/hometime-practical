cdbde62ff65d6757647e7c3b4a5e1674a94d4a7a
itchan <christianbaman4@gmail.com>
2023-01-11 16:44:55 +0800

    Changed some error message

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,6 +6,7 @@ class Reservation < ApplicationRecord
   validates :code, :status, :currency, :total_amount, :adults, :end_date, :start_date,
             :guest, :nights, :security_price, :payout_price, presence: true
   validates :code, uniqueness: true
+  validates :adults, numericality: { greater_than_or_equal_to: 0 }
   validates :total_amount, numericality: { greater_than_or_equal_to: 0 }
   validate :duration_validation
   validate :number_of_guest_validation
@@ -21,21 +22,21 @@ class Reservation < ApplicationRecord
   def number_of_guest_validation
     total_guests = (adults + children + infants)
     if total_guests != guests
-      errors.add(:guest, "Guest number should be equal to total number of adults to infants")
+      errors.add(:guest, "number should be equal to total number of adults to infants")
     end
   end
 
   def number_of_nights_validation
     total_nights = (end_date.to_date - start_date.to_date).to_i
     if total_nights != nights
-      errors.add(:night, "Total night number should be equal to duration of reservation")
+      errors.add(:night, "number should be equal to the duration of end_date and start_date")
     end
   end
 
   def total_amount_validation
     total_price = (security_price + payout_price)
     if total_price != total_amount
-      errors.add(:total_amount, "Total amount is wrong please calculate it again")
+      errors.add(:total_amount, "is wrong please check the security price and payout price")
     end
   end
 
